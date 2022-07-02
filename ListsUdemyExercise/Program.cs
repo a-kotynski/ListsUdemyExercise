b3b59085aed1494717a28a6eef799bebeddd2289
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-02 06:54:17 +0200

    link

--- ListsUdemyExercise/Program.cs
+++ ListsUdemyExercise/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//https://www.udemy.com/course/csharp-tutorial-for-beginners/learn/lecture/2979538#content
 namespace ListsUdemyExercise
 {
     class Program
